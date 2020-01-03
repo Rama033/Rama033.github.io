bc131b4a025005b0ef7448ca45b15b3f18e30d68
Rama033 <vfjdg12@gmail.com>
2020-01-03 14:18:42 +0900

    print log when automation process completed

--- update.sh
+++ update.sh
@@ -2,8 +2,16 @@
 
 echo `git add _config.yml ./_posts/`
 
+echo "\n\nadd _config.yml and posts complete\n\n"
+
 echo `git commit -m "$1"`
+
+echo "\n\ncommit with message \"$1\"\n\n"
 
 echo `git push origin raw`
 
+echo "\n\npush to origin/raw\n\n"
+
 echo `rake publish`
+
+echo "\n\nfinshed publishing\n\n"
