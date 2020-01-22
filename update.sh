8a48e76bce91154a080490e04cd3ea0530533bf1
Rama033 <vfjdg12@gmail.com>
2020-01-22 14:31:32 +0900

    post update 자동화 로그 문구 변경

--- update.sh
+++ update.sh
@@ -2,16 +2,16 @@
 
 echo `git add _config.yml ./_posts/`
 
-echo -e "\n\nadd _config.yml and posts complete\n\n"
+echo -e "\n\n...complete add _config.yml and posts...\n\n"
 
 echo `git commit -m "$1"`
 
-echo -e "\n\ncommit with message \"$1\"\n\n"
+echo -e "\n\n...complete commit with message \"$1\"...\n\n"
 
 echo `git push origin raw`
 
-echo -e "\n\npush to origin/raw\n\n"
+echo -e "\n\n...complete push to origin/raw...\n\n"
 
 echo `rake publish`
 
-echo -e "\n\nfinshed publishing\n\n"
+echo -e "\n\n...complete publishing...\n\n"
