b6a9fcd5f873e1cf2d50b43af7f349b596b8e5c5
sungun.jo <vfjdg12@gmail.com>
2020-05-26 15:45:15 +0900

    v 1.6.4 update.sh 수정, 이미지 파일 추가

--- update.sh
+++ update.sh
@@ -2,16 +2,16 @@
 
 echo `git add _config.yml ./_posts/`
 
-echo -e "\n\n...complete add _config.yml and posts...\n\n"
+echo  "\n\n...complete add _config.yml and posts...\n\n"
 
 echo `git commit -m "$1"`
 
-echo -e "\n\n...complete commit with message \"$1\"...\n\n"
+echo  "\n\n...complete commit with message \"$1\"...\n\n"
 
 echo `git push origin raw`
 
-echo -e "\n\n...complete push to origin/raw...\n\n"
+echo  "\n\n...complete push to origin/raw...\n\n"
 
 echo `rake publish`
 
-echo -e "\n\n...complete publishing...\n\n"
+echo  "\n\n...complete publishing...\n\n"
