583befbc3bca8a9d9f586a6e3fab2a0167b11523
sungun.jo <sungun.jo@webtoonscorp.com>
2022-04-12 22:29:27 +0900

    Site updated at 2022-04-12 22:29:26 UTC+9:00

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo `git add _config.yml ./_posts/ ./assets/`
+
+echo  "\n\n...complete add _config.yml and posts...\n\n"
+
+echo `git commit -m "$1"`
+
+echo  "\n\n...complete commit with message \"$1\"...\n\n"
+
+echo `git push origin raw`
+
+echo  "\n\n...complete push to origin/raw...\n\n"
+
+echo `rake publish`
+
+echo  "\n\n...complete publishing...\n\n"
